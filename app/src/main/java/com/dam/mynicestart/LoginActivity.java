eae7866571b641bdfe0a2f909fbcf8d5da734b35
Nachoj8 <nachojimenezr8@gmail.com>
2020-11-13 13:22:19 +0100

    Splash

--- app/src/main/java/com/dam/mynicestart/LoginActivity.java
+++ app/src/main/java/com/dam/mynicestart/LoginActivity.java
@@ -2,13 +2,52 @@ package com.dam.mynicestart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    protected TextView mSignUpTextview;
+    protected Button mLoginButton;
+    protected TextInputLayout mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        mPassword = (TextInputLayout)findViewById(R.id.password);
+//        mPassword.requestFocus();
+
+
+        mLoginButton = (Button)findViewById(R.id.button1);
+        mLoginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+
+
+            }
+        });
+
+        mSignUpTextview = (TextView)findViewById(R.id.signup_text);
+        mSignUpTextview.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
