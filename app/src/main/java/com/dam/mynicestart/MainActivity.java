c0c1cea2a95ced6145856691919c89b56789c675
Nachoj8 <nachojimenezr8@gmail.com>
2020-11-27 13:46:23 +0100

    scrim

--- app/src/main/java/com/dam/mynicestart/MainActivity.java
+++ app/src/main/java/com/dam/mynicestart/MainActivity.java
@@ -20,4 +20,3 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-//prueba23
--- app/src/main/java/com/dam/mynicestart/SplashActivity.java
+++ app/src/main/java/com/dam/mynicestart/SplashActivity.java
@@ -3,11 +3,17 @@ package com.dam.mynicestart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -23,6 +29,19 @@ public class SplashActivity extends AppCompatActivity {
         mySubtitle.startAnimation(myanim);
 
         openApp(true);
+
+        ImageView  mSurf = findViewById(R.id.surf);
+
+        Glide.with(this)
+                .load(R.drawable.surf)
+                .centerCrop()
+                //.transition(DrawableTransitionOptions.withCrossFade(500))
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.colorAccent)))
+               // .circleCrop()
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .into(mSurf);
+
+      // Para cargar imagen de internet en .load  "https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80"
     }
 
 
@@ -35,6 +54,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 5000);
+        }, 6000);
     }
 }
