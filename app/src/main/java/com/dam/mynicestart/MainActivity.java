41b69444d6311fe60023fbb06f15c7f48e48bf31
Nachoj8 <nachojimenezr8@gmail.com>
2021-01-29 10:42:13 +0100

    Ultimos cambios

--- app/src/main/java/com/dam/mynicestart/MainActivity.java
+++ app/src/main/java/com/dam/mynicestart/MainActivity.java
@@ -1,15 +1,28 @@
 package com.dam.mynicestart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.alespero.expandablecardview.ExpandableCardView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.material.snackbar.Snackbar;
 
 /**
  * Ventana MainActivity
@@ -22,12 +35,21 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView img;
 
+    private SwipeRefreshLayout swipeLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ImageView imgMain = findViewById(R.id.img);
+
+        ImageView mycontext = (ImageView) findViewById(R.id.img);
+        registerForContextMenu(mycontext);
+
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
+
 
         String url = ("https://bucket3.glanacion.com/anexos/fotos/67/3256867.jpg");
 
@@ -41,11 +63,142 @@ public class MainActivity extends AppCompatActivity {
                 .into(imgMain);
 
         // Para cargar imagen de internet en .load  "https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80"
+
+
+        // *** EXPANDABLE CARD ***
+        ExpandableCardView card = findViewById(R.id.profile);
+        card.setOnExpandedListener(new ExpandableCardView.OnExpandedListener() {
+            @Override
+            public void onExpandChanged(View v, boolean isExpanded) {
+                Toast.makeText(MainActivity.this, isExpanded ? "Expanded!" :
+                        "Collapsed!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    protected SwipeRefreshLayout.OnRefreshListener
+            mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            /*Toast toast0 = Toast.makeText(MainActivity.this, "going swipeREFRESH", Toast.LENGTH_LONG);
+                    toast0.show();*/
+
+            final ConstraintLayout mLayout = (ConstraintLayout) findViewById(R.id.activity_main_context);
+
+            Snackbar snackbar = Snackbar
+                    .make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
+                    .setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+                            snackbar1.show();
+                        }
+                    });
+            snackbar.show();
+            swipeLayout.setRefreshing(false);
+        }
+    };
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.edit:
+                Toast toast = Toast.makeText(this, "going CONTEXT CAMERA", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+
+            case R.id.delete:
+                Toast toast2 = Toast.makeText(this, "going CONTEXT SETTINGS", Toast.LENGTH_SHORT);
+                toast2.show();
+                break;
+
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+
+        } else if (id == R.id.camera) {
+            Toast toast = Toast.makeText(this, "going APPBAR CAMERA", Toast.LENGTH_LONG);
+            toast.show();
+            showAlertDialogButtonClicked(MainActivity.this);
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void main2(View view) {
         Intent intent = new Intent(MainActivity.this, MainActivity2.class);
         startActivity(intent);
     }
+
+    // *** DIALOGO MODAL ***
+    public void showAlertDialogButtonClicked(MainActivity view) {
+        // setup the alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //el dialogo estandar tiene título/icono pero podemos sustituirlo
+        // por un XML a medida
+        // builder.setTitle("Achtung!");
+        // builder.setMessage("Where do you go?");
+        // builder.setIcon(R.drawable.ic_action_name_dark);
+        // un XML a medida para el diálogo
+
+        builder.setView(getLayoutInflater().inflate(R.layout.alertdialog_view,
+                null));
+
+        // add the buttons
+        builder.setPositiveButton("Glide", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do something like...
+                        dialog.dismiss();
+                    }
+                });
+        builder.setNegativeButton("ChatBot", new
+                DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do something like...
+                        dialog.dismiss();
+                    }
+                });
+
+        builder.setNeutralButton("MotionLayout", new
+                DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do something like...
+                        dialog.dismiss();
+                    }
+                });
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
 
