c39d639c03aaf5227828192cc0f8ffa41c928e99
Nachoj8 <nachojimenezr8@gmail.com>
2021-02-19 11:03:30 +0100

    Test

--- app/src/androidTest/java/com/dam/mynicestart/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/dam/mynicestart/ExampleInstrumentedTest.java
@@ -4,11 +4,21 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +27,34 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(
+            LoginActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.dam.mynicestart", appContext.getPackageName());
     }
+
+//    @Test
+//    public void textInputOutput() {
+//        onView(withId(R.id.username)).perform(
+//                typeText("This is a test."));
+//       onView(withId(R.id.button1)).perform(click());
+//       onView(withId(R.id.password)).check(
+//               matches(withText("This is a test.")));
+//    }
+
+    @Test
+    public void activityLaunch() {
+        onView(withId(R.id.button1)).perform(click());
+//        onView(withId(R.id.text_header)).check(matches(isDisplayed()));
+//        onView(withId(R.id.button_second)).perform(click());
+//        onView(withId(R.id.text_header_reply)).check(matches(isDisplayed()));
+    }
+
+
 }
